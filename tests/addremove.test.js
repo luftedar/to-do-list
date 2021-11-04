84968581dbdba6c7b6cf8b8f95e6f58f05e5082c
sja_thedude <sja.affu765@gmail.com>
2021-11-04 11:45:37 +0300

    Updated with linter errors

--- tests/addremove.test.js
+++ tests/addremove.test.js
@@ -1,4 +1,4 @@
-const { addNewItem,removeItem } = require("../src/__mocks__/addremove")
+const { addNewItem, removeItem } = require('../src/__mocks__/addremove');
 
 const taskList = [
   {
@@ -11,17 +11,17 @@ const taskList = [
     completed: false,
     index: 1,
   },
-]
+];
 
 describe('Tests for add remove', () => {
   test('Add Item', () => {
-    addNewItem('New Task',false,taskList.length,taskList)
+    addNewItem('New Task', false, taskList.length, taskList);
     expect(taskList.length).toBe(3);
   });
   test('Remove Item', () => {
-    expect(removeItem(1,taskList)).toBe(2);
+    expect(removeItem(1, taskList)).toBe(2);
   });
   test('Check The Index', () => {
     expect(taskList[1].index).toBe(1);
-  })
-})
+  });
+});
